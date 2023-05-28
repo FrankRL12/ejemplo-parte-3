c46a32528c2048e1b9a409cd68498c1ca1ad07fd
FrankRL12 <ramirezlujanojosefrancisco4@gmail.com>
2023-05-28 00:08:33 -0600

    commit

--- libreria/settings.py
+++ libreria/settings.py
@@ -107,10 +107,10 @@ WSGI_APPLICATION = 'libreria.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        conn_max_age=600,
-        conn_health_checks=True,
-    ),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 
