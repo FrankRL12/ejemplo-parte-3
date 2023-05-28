e12b669c32141717bb1db7b1610db37f30b42ffa
FrankRL12 <ramirezlujanojosefrancisco4@gmail.com>
2023-05-28 00:23:29 -0600

    commit

--- libreria/urls.py
+++ libreria/urls.py
@@ -20,6 +20,6 @@ from django.views.generic import RedirectView
 from django.conf.urls.static import static
 
 urlpatterns = [
-     path('', RedirectView.as_view(url='/admin/')),
+    path('', RedirectView.as_view(url='/admin/')),
     path('admin/', admin.site.urls),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
